09f16982ce9745f6c1d0cac9fc08b4fd9761880d
kayabahri <kayaabahri@gmail.com>
2024-03-21 13:09:20 +0300

    firebase sorunu

---
+++ message/firebaseConfig.js
@@ -0,0 +1,12 @@
+// firebaseConfig.js
+const firebaseConfig = {
+    apiKey: "AIzaSyDZ9VWM7sQo0QRbPCvSnRRJPjn3muMj4cg",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "message-76a6d",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+  };
+  
+  export default firebaseConfig;
+  
